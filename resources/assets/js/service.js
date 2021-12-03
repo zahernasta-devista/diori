988f5586de19e751d6132e81ee9504a1c53f26b4
Dana Aljafari <aljafaridana@yahoo.com>
2021-12-03 10:23:07 +0200

    Resolving date issues

--- resources/assets/js/service.js
+++ resources/assets/js/service.js
@@ -1,18 +1,3 @@
 $(document).ready(function() {
-    function getProject(selectedProject) {
 
-        let projectId = selectedProject.value;
-    
-        $.ajax({
-            url: url,
-            success: function (result) {
-                let projectStartDate = result.project.start_date;
-                $("#date").attr({
-    
-                    "min": projectStartDate,
-    
-                });
-            }
-        });
-    }
 });
