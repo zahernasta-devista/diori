3f9ce68a5d94530bafb7daea01514355ee835965
meizan19 <90834031+MazenALwan@users.noreply.github.com>
2021-12-16 11:49:30 +0200

    resolved conversation

--- public/assets/js/fullcalendar.js
+++ public/assets/js/fullcalendar.js
@@ -10,7 +10,6 @@ $(function (e) {
     type: 'GET',
     url: url,
     success: function success(response) {
-      console.log('Done');
       var responseData = response.response;
       var events = [];
       var time = {
