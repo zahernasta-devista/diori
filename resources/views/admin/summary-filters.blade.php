703f3c0c1ae57b38cbe1ff3ae9a774f0467d9281
sham <you@example.com>
2021-12-22 10:35:39 +0200

    FEATURE/DW-146-Summary-page

--- resources/views/admin/summary-filters.blade.php
+++ resources/views/admin/summary-filters.blade.php
@@ -24,7 +24,7 @@
                 <div class="card-body">
                     <div class="d-flex">
                         <div class="text-white">
-                            <h2 class="mb-0 number-font">Total Hours Worked On Selected Project:</h2>
+                            <h3 class="mb-0 number-font">Total Hours Worked On Selected Project:</h3>
                             <h3 class="text-white mb-0">•{{$sumPerSelectedProject}} Hours</h3>
                         </div>
                         <div class="ml-auto"> <i class="fa fa-pencil text-white fs-30 mr-2 mt-2"></i> </div>
@@ -37,7 +37,7 @@
                 <div class="card-body">
                     <div class="d-flex">
                         <div class="text-white">
-                            <h2 class="mb-0 number-font">Overall Hours Worked This Month:</h2>
+                            <h3 class="mb-0 number-font">Overall Hours Worked This Month:</h3>
                             <h3 class="text-white mb-0">
                                 •{{$overallSum}} Hours</h3>
                         </div>
@@ -57,31 +57,37 @@
                         <div class="card-options">
                             <select name="month"  class="btn btn-md btn-purple-gradient rounded-pill"><i class="fa fa-dropbox" required></i>
                                 <option  selected disabled hidden>Select The Month</option>
-                                <option class="btn btn-purple" value="1">January</option>
-                                <option class="btn btn-purple" value="2">February</option>
-                                <option class="btn btn-purple" value="3">March</option>
-                                <option class="btn btn-purple" value="4">April</option>
-                                <option class="btn btn-purple" value="5">May</option>
-                                <option class="btn btn-purple" value="6">June</option>
-                                <option class="btn btn-purple" value="7">July</option>
-                                <option class="btn btn-purple" value="8">August</option>
-                                <option class="btn btn-purple" value="9">September</option>
-                                <option class="btn btn-purple" value="10">October</option>
-                                <option class="btn btn-purple" value="11">November</option>
-                                <option class="btn btn-purple" value="12">December</option>
+                                <option class="btn btn-purple" value="1" @if(request()->month =='1') selected @endif>January</option>
+                                <option class="btn btn-purple" value="2" @if(request()->month =='2') selected @endif>February</option>
+                                <option class="btn btn-purple" value="3" @if(request()->month =='3') selected @endif>March</option>
+                                <option class="btn btn-purple" value="4" @if(request()->month =='4') selected @endif>April</option>
+                                <option class="btn btn-purple" value="5" @if(request()->month =='5') selected @endif>May</option>
+                                <option class="btn btn-purple" value="6" @if(request()->month =='6') selected @endif>June</option>
+                                <option class="btn btn-purple" value="7" @if(request()->month =='7') selected @endif >July</option>
+                                <option class="btn btn-purple" value="8" @if(request()->month =='8') selected @endif>August</option>
+                                <option class="btn btn-purple" value="9" @if(request()->month =='9') selected @endif>September</option>
+                                <option class="btn btn-purple" value="10" @if(request()->month =='10') selected @endif>October</option>
+                                <option class="btn btn-purple" value="11" @if(request()->month =='11') selected @endif>November</option>
+                                <option class="btn btn-purple" value="12" @if(request()->month =='12') selected @endif>December</option>
                             </select>
                             &emsp;
                             <select  name="year"  class="btn btn-md btn-purple-gradient rounded-pill"><i class="fa fa-dropbox" ></i>
                                 <option value="" selected disabled hidden>Select The Year</option>
                                 @foreach(range(2021, date('Y')+20) as $y)
-                                    <option value ="{{$y}}" class="btn btn-purple">{{$y}}</option>
+                                    <option value ="{{$y}}" class="btn btn-purple" @if(request()->year ==$y) selected @endif>{{$y}}</option>
                                 @endforeach
                             </select>
                             &emsp;
+                            @php
+                            use Carbon\Carbon;
+                                $currentMonth = Carbon::now()->format('m');
+                                $currentYear = Carbon::now()->format('Y');
+                         @endphp
                             <select  name="project"  class="btn btn-md btn-purple-gradient rounded-pill"><i class="fa fa-dropbox" ></i>
-                                @foreach($projectsOptions as $project)
                                 <option selected disabled hidden>Select The Project</option>
-                                    <option class="btn btn-purple" value="{{$project->id}}">{{$project->name}}</option>
+                                <option class="btn btn-purple" value="">All Projects</option>
+                                @foreach($projectsOptions as $project)
+                                    <option class="btn btn-purple" value="{{$project->id}}" @if(request()->project ==$project->id) selected @endif>{{$project->name}}</option>
                                 @endforeach
                             </select>
                             &emsp;
@@ -95,7 +101,6 @@
                                     {{$errors->first()}}
                                 </div>
                             @endif
-{{--                            <div class="alert alert-primary"> The Date selected {{$selectYear}}/{{$selectMonth}}</div>--}}
                             <table id="example" class="table table-striped table-bordered text-nowrap w-100">
                                 <thead>
                                 <tr>
