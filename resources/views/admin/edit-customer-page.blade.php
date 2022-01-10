b49c88e6f289ba86265385ed744998281cb9f267
MazenALwan <90834031+MazenALwan@users.noreply.github.com>
2022-01-10 03:35:51 -0800

    QA/DW-169-misspelled-word (#101)

--- resources/views/admin/edit-customer-page.blade.php
+++ resources/views/admin/edit-customer-page.blade.php
@@ -43,7 +43,7 @@
                         </div>
                         <div class="container-login100-form-btn">
                             <button  class="login100-form-btn btn-purple-gradient">
-                                {{ __('Sumbit') }}
+                                {{ __('Submit') }}
                             </button>
                         </div>
                     </form>
