fcfc54dc81856f22c96a9d29013f74a31aed3d09
Dana Aljafari <aljafaridana@yahoo.com>
2022-01-14 14:26:18 +0200

    QA/Improvements-ResetPassword-Mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,5 @@
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
-</a>
+<a href="{{ $url }}" style="display: inline-block;"></a>
 </td>
 </tr>
