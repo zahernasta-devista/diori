09ba6370bad987955291e12e451fb079d889b2cc
sham <you@example.com>
2022-01-20 14:08:06 +0200

    QA/REFACTOR-BUGS

--- resources/views/layouts/vertical-menu-employee/app-sidebar.blade.php
+++ resources/views/layouts/vertical-menu-employee/app-sidebar.blade.php
@@ -1,17 +1,17 @@
 <!--APP-SIDEBAR-->
-<a aria-label="Hide Sidebar" class="app-sidebar__toggle ml-auto" data-toggle="sidebar" href="#"></a><!-- sidebar-toggle-->
+<a aria-label="Hide Sidebar" class="app-sidebar__toggle ml-auto" data-toggle="sidebar" id="sidebarbutton" href="#"></a><!-- sidebar-toggle-->
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 <aside class="app-sidebar">
     <div class="side-header">
         <a class="header-brand1" href="{{ url('/' . ($page = 'dashboard')) }}">
             <h2 class="text-purple-gradient">Diori</h2>
-            <a aria-label="Hide Sidebar" class="app-sidebar__toggle ml-auto" data-toggle="sidebar" href="#"></a>
+            <a aria-label="Hide Sidebar" class="app-sidebar__toggle ml-auto" data-toggle="sidebar"  id="sidebarbutton" href="#"></a>
             <!-- sidebar-toggle-->
     </div>
     <div class="app-sidebar__user">
         <div class="dropdown user-pro-body text-center">
             <div class="user-info">
-                <h6 class=" mb-0 text-dark"><i class="fa fa-user-circle-o fa-sm text-black fs-30 mr-2 mt-2"></i>{{ Auth::user()->name }}</h6>
+                <h6 class=" mb-0 text-dark"><i id="profilepicture" class="fa fa-user-circle-o fa-sm text-black fs-30 mr-2 mt-2"></i>{{ Auth::user()->name }}</h6>
                 <span class="text-muted app-sidebar__user-name text-sm">{{ Auth::user()->position }}</span>
             </div>
         </div>
